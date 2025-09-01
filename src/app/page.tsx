8831371ec312a7d0f726d69a07ad70a13f9af53e
Subhojit0012 <subhojitroy00012@gmail.com>
2025-09-01 18:09:39 +0530

    marquee implementaion has done

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,6 +5,13 @@ import { Button } from "@/components/ui/button";
 import { Send } from "lucide-react";
 import { Mail } from "lucide-react";
 import Link from "next/link";
+import Client3 from "../../public/img/profile/client-3.webp";
+import {
+  Marquee,
+  MarqueeItem,
+  MarqueeFade,
+  MarqueeContent,
+} from "@/components/ui/kibo-ui/marquee";
 
 export default function Home() {
   return (
@@ -12,11 +19,9 @@ export default function Home() {
       <header className="w-full flex flex-col items-center justify-between mx-auto">
         <nav className="flex w-full items-center justify-between mx-auto px-4">
           <Image
-            src={"/img/profile/client-3.webp"}
+            src={Client3}
             alt="Profile Image"
-            width={60}
-            height={5}
-            className="rounded-full object-cover h-15"
+            className="rounded-full object-cover h-16 w-16"
           />
 
           <div className="">
@@ -70,6 +75,47 @@ export default function Home() {
           </div>
         </section>
       </header>
+
+      {/* dev stack section */}
+      <div className="w-full flex size-full items-center justify-center">
+        <Marquee>
+          <MarqueeFade side="left" />
+          <MarqueeFade side="right" />
+          <MarqueeContent>
+            {new Array(10).fill(null).map((_, index) => (
+              <MarqueeItem className="h-32 w-32" key={index}>
+                <Image
+                  src={`https://placehold.co/128x128?random=${index}`}
+                  alt={`Placeholder ${index}`}
+                  className="overflow-hidden rounded-full"
+                  width={100}
+                  height={100}
+                  unoptimized
+                />
+              </MarqueeItem>
+            ))}
+          </MarqueeContent>
+          <MarqueeContent direction="right">
+            {new Array(10).fill(null).map((_, index) => (
+              <MarqueeItem className="h-32 w-32" key={index}>
+                <Image
+                  src={`https://placehold.co/128x128?random=${index}`}
+                  alt={`Placeholder ${index}`}
+                  className="overflow-hidden rounded-full"
+                  width={100}
+                  height={100}
+                  unoptimized
+                />
+              </MarqueeItem>
+            ))}
+          </MarqueeContent>
+        </Marquee>
+      </div>
+
+      {/* Project section */}
+      <div className="h-screen w-full bg-purple-700 grid-cols-2 grid-rows-2 gap-4 mt-5">
+        <h2>Project section</h2>
+      </div>
     </div>
   );
 }
