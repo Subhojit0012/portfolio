1d45c552fe83e760982e484141ac8c01396beada
Subhojit0012 <subhojitroy00012@gmail.com>
2025-09-03 09:04:33 +0530

    infinite moving cards are added for testimonial

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -12,8 +12,40 @@ import {
   MarqueeFade,
   MarqueeContent,
 } from "@/components/ui/kibo-ui/marquee";
+import { InfiniteMovingCards } from "@/components/ui/infinite-moving-cards";
 
 export default function Home() {
+  const testimonials = [
+    {
+      quote:
+        "It was the best of times, it was the worst of times, it was the age of wisdom, it was the age of foolishness, it was the epoch of belief, it was the epoch of incredulity, it was the season of Light, it was the season of Darkness, it was the spring of hope, it was the winter of despair.",
+      name: "Charles Dickens",
+      title: "A Tale of Two Cities",
+    },
+    {
+      quote:
+        "To be, or not to be, that is the question: Whether 'tis nobler in the mind to suffer The slings and arrows of outrageous fortune, Or to take Arms against a Sea of troubles, And by opposing end them: to die, to sleep.",
+      name: "William Shakespeare",
+      title: "Hamlet",
+    },
+    {
+      quote: "All that we see or seem is but a dream within a dream.",
+      name: "Edgar Allan Poe",
+      title: "A Dream Within a Dream",
+    },
+    {
+      quote:
+        "It is a truth universally acknowledged, that a single man in possession of a good fortune, must be in want of a wife.",
+      name: "Jane Austen",
+      title: "Pride and Prejudice",
+    },
+    {
+      quote:
+        "Call me Ishmael. Some years ago—never mind how long precisely—having little or no money in my purse, and nothing particular to interest me on shore, I thought I would sail about a little and see the watery part of the world.",
+      name: "Herman Melville",
+      title: "Moby-Dick",
+    },
+  ];
   return (
     <div className="w-full h-full flex flex-col items-center justify-center max-w-7xl mx-auto">
       <header className="w-full flex flex-col items-center justify-between mx-auto">
@@ -113,9 +145,26 @@ export default function Home() {
       </div>
 
       {/* Project section */}
-      <div className="h-screen w-full bg-purple-700 grid-cols-2 grid-rows-2 gap-4 mt-5">
+      <div className="h-screen w-full bg-purple-700 mt-5">
         <h2>Project section</h2>
+        <div className="grid grid-cols-2 grid-rows-2 gap-4 mx-auto w-full h-full">
+          <div className="h-full w-full bg-amber-50 border rounded-2xl"></div>
+          <div className="h-full w-full bg-amber-50 border rounded-2xl"></div>
+          <div className="h-full w-full bg-amber-50 border rounded-2xl"></div>
+          <div className="h-full w-full bg-amber-50 border rounded-2xl"></div>
+        </div>
       </div>
+
+      <div className="h-[40rem] rounded-md flex flex-col antialiased items-center justify-center relative overflow-hidden">
+        <InfiniteMovingCards
+          items={testimonials}
+          direction="right"
+          speed="fast"
+          pauseOnHover={false}
+        />
+      </div>
+
+      
     </div>
   );
 }
